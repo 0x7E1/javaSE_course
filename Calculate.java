b039aba0c8f8b353c759c2cb0add30db033edec0
VAIO <vladislaw.volkov@gmail.com>
2017-02-04 01:48:43 +0200

    added logic in program

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,17 @@
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Calculate...");
+		int firstNum = Integer.valueOf(args[0]);
+		int secondNum = Integer.valueOf(args[1]);
+		int addition = firstNum + secondNum;
+		int substraction = firstNum - secondNum;
+		int multiplication = firstNum * secondNum;
+		double division = (float)firstNum / (float)secondNum;
+		double involution = Math.pow((float)firstNum, (float)secondNum);
+		System.out.println("Addition = " + addition);
+		System.out.println("Substraction = " + substraction);
+		System.out.println("Multiplication = " + multiplication);
+		System.out.println("Division = " + division);
+		System.out.println("Involution = " + involution);
 	}
 }
