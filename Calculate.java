8f26df525f755c2c9ddf157911b9a1d036949b87
VAIO <vladislaw.volkov@gmail.com>
2017-02-04 01:51:34 +0200

    added comments in code

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,8 @@
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Calculate...");
+
+		// Создаем переменные на каждую операцию
 		int firstNum = Integer.valueOf(args[0]);
 		int secondNum = Integer.valueOf(args[1]);
 		int addition = firstNum + secondNum;
@@ -8,6 +10,8 @@ public class Calculate {
 		int multiplication = firstNum * secondNum;
 		double division = (float)firstNum / (float)secondNum;
 		double involution = Math.pow((float)firstNum, (float)secondNum);
+
+		// Выводим результаты операций
 		System.out.println("Addition = " + addition);
 		System.out.println("Substraction = " + substraction);
 		System.out.println("Multiplication = " + multiplication);
